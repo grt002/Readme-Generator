d8f21634e6f9ee97e718ac4afeca9d34c9e5020f
Garrett <grt002@outlook.com>
2023-04-08 11:20:41 -0500

    Added fields to function to write readme

--- index.js
+++ index.js
@@ -56,13 +56,47 @@ inquirer.prompt(questions).then(function (answers) {
     console.log(answers);
 
     // Function to write README file
-    function writeToFile(fileName, data); {
+    function writeToFile(fileName, data) {
         fs.writeFile(fileName, data, function (err) {
             if (err) {
                 return console.log(err);
             }
             console.log("README.md file generated successfully!");
         });
+    }
+
+    function generateMarkdown(data) {
+        return `# ${data.Title}
+
+        ## Description
+        ${data.Description}
+
+        ## Table of Contents
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [License](#license)
+        - [Contributing](#contributing)
+        - [Tests](#tests)
+        - [Questions](#questions)
+
+        ## Installation
+        ${data.Installation}
+
+        ## Usage
+        ${data.Usage}
+
+        ## License
+        This project is licensed under the ${data.License} license.
+
+        ## Contributing
+        ${data.Contributing}
+
+        ## Tests
+        ${data.Tests}
+
+        ## Questions
+        If you have any questions, please feel free to contact me through my GitHub profile [${data.GitHub}](https://github.com/${data.GitHub}) or via email at ${data.email}.
+        `;
     }
 
     writeToFile('README.md', generateMarkdown(answers));
